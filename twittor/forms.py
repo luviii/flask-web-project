780bf40919d26568cd3bb9837b3aec4d0ec91de2
luvi <ljing1027@gmail.com>
2020-03-24 12:47:01 -0400

    create project

---
+++ twittor/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
+
+from twittor.models import User
+
+WTF_CSRF_ENABLED = False
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email Address", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField("Password Repeat", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('please use different username')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('please use different email address')
+
+class EditProfileForm(FlaskForm):
+    about_me = TextAreaField("About me", validators=[Length(min=0, max=128)])
+    submit = SubmitField('save')
---
+++ twittor/route.py
@@ -0,0 +1,88 @@
+from flask import render_template, redirect, url_for, request, abort
+from flask_login import login_user, current_user, logout_user, login_required
+from twittor.forms import LoginForm, RegisterForm, EditProfileForm
+from twittor.models import User, Tweet
+from twittor import db
+
+@login_required
+def index():
+    
+    posts = [
+        {
+            'author':{'username':'root'},
+            'body':"Hi I'm root"
+        },
+        {
+            'author':{'username': 'test'},
+            'body': "Hi I'm test"
+        },
+        {
+            'author':{'username': 'test1'},
+            'body': "Hi I'm test1"
+        }
+        
+    ]
+    return render_template('index.html', posts = posts)
+
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm(csrf_enable=False)
+    if form.validate_on_submit():
+        u = User.query.filter_by(username=form.username.data).first()
+        if u is None or not u.check_password(form.password.data):
+            print('invalid username or password')
+            return redirect(url_for('login'))
+        login_user(u, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if next_page:
+            return redirect(next_page)
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Sign In", form=form)
+
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user =  User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Registeration', form=form)
+
+@login_required
+def user(username):
+    u = User.query.filter_by(username=username).first()
+    if u is None:
+        abort(404)
+    posts = [
+        {
+            'author':{'username':u.username},
+            'body':"Hi I'm {}!".format(u.username)
+        },
+        {
+            'author':{'username':u.username},
+            'body': "Hi I'm {}!".format(u.username)
+        }    
+    ]
+    return render_template('user.html', title='Profile',posts=posts, user=u)
+
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if request.method == 'GET':
+        form.about_me.data = current_user.about_me
+    if form.validate_on_submit():
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        return redirect(url_for('profile',username=current_user.username))
+    return render_template('edit_profile.html', form=form)
